7f7c43982ba77b1a0beb180a7dbae7b725d6cd82
Vitali Olshevski <tech@olshevski.dev>
2021-12-09 17:18:01 +0300

    Add required POM information

--- buildSrc/src/main/kotlin/publishing-config.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-config.gradle.kts
@@ -14,6 +14,34 @@ afterEvaluate {
         publications {
             create<MavenPublication>(createPublicationName()) {
                 from(components.findByName("release") ?: components.findByName("java")!!)
+
+                pom {
+                    name.set("Compose Safe Args")
+                    description.set("Missing safe arguments generator for Android Compose Navigation")
+                    url.set("https://github.com/olshevski/compose-safe-args")
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/olshevski/compose-safe-args/blob/main/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("olshevski")
+                            name.set("Vitali Olshevski")
+                            email.set("tech@olshevski.dev")
+                            url.set("https://olshevski.dev")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:https://github.com/olshevski/compose-safe-args.git")
+                        developerConnection.set("scm:git:https://github.com/olshevski/compose-safe-args.git")
+                        url.set("https://github.com/olshevski/compose-safe-args")
+                    }
+                }
             }
         }
     }
